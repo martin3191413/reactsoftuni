5dc607d3acc6195275c85f2dac2d706eec67ca5e
martin3191413 <75796586+martin3191413@users.noreply.github.com>
2021-03-03 12:49:37 +0200

    Starting  with the products page

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="logo">
+                mySite
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Login</li>
+            </ul>
+            <div className="search">
+            <i className="fa fa-search"></i>
+         <i className="fa fa-shopping-basket"></i>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
