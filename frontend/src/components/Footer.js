2674d431bbd29bbc62ab4116e4837ded28193c8f
martin3191413 <75796586+martin3191413@users.noreply.github.com>
2021-03-03 16:17:12 +0200

    Some hover effects and added footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <span>Martin Chakarov Softuni React Course March 2021</span>
+            <span>All Rights Reserved ©</span>
         </div>
     );
 };
--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -3,13 +3,16 @@ import React from 'react';
 import product_card from './product_data';
 
 const MainContent = () => {
+
     const listItems = product_card.map((item) =>
     <div className="card" key={item.id}>
-        <h3>{item.name}</h3>
         <img className="img" src={item.thumb} alt="item "></img>
+        <h3>{item.product_name}</h3>
         <p>{item.description}</p>
         <span className="price">{item.price}</span>
-        <div className="btn">Add to Cart</div>
+        <div className="btn">
+            <span className="btn btn-text">Add to Cart</span>
+        </div>
     </div>
     );
 
